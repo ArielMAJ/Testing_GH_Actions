1de88af5f674ccd69776040c9b46c4ade1cb5d34
ArielMAJ <ariel.maj@hotmail.com>
2022-05-08 20:12:19 -0300

    Created a reusable workflow for assisting in testing a given folder. Created a workflow for using the reusable one and test a folder that was just added.

---
+++ SomeAssignment/Q01/solution.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int times_two(int value);
+
+int main(void)
+{
+	int value;
+	scanf("%d", &value);
+
+	printf("%d\n", times_two(value));
+	return 0;
+}
+
+int times_two(int value)
+{
+	return value * 2;
+}
+
